8a78b00a980e1f6bf1c1a311bdf2b59b8dc47d7b
gregsharp <gregsharp@8226ee30-1aee-0310-a633-b68f58d1eb45>
2003-01-18 21:46:09 +0000

    Update scripts and notes on how to make windows version.
    git-svn-id: svn://svn.berlios.de/lincity-ng/trunk@140 8226ee30-1aee-0310-a633-b68f58d1eb45

---
+++ tools/delete_cruft.bat
@@ -0,0 +1,38 @@
+deltree /y CVS
+erase .cvsignore
+erase ABOUT-NLS
+erase Makefile*
+erase *.h
+erase *.c
+erase *.m4
+deltree /y archive
+erase *.in
+erase *.sh
+erase configure.*
+erase *-sh
+erase *.6
+erase *.ico
+erase *.ico
+erase *.rc
+erase missing
+erase mkinstalldirs
+deltree /y help\cvs
+deltree /y icons\cvs
+deltree /y intl
+deltree /y messages\cvs
+deltree /y old.Makefiles
+deltree /y opening\cvs
+deltree /y po
+erase *.mak
+deltree /y lib
+deltree /y debug
+erase *.ncb
+erase *.mdp
+erase *.plg
+erase *.dsp
+deltree /y SAVED_GAMES
+erase *.opt
+deltree /y release
+erase *.diff
+erase *.dsw
+deltree /y tools
