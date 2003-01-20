73f7a58937f9be75d063d3fe9ef7ef67fc6090e0
gregsharp <gregsharp@8226ee30-1aee-0310-a633-b68f58d1eb45>
2003-01-20 17:04:25 +0000

    More fixes related to new version of autoconf/automake. New script for unixifying DOS formatted files.
    git-svn-id: svn://svn.berlios.de/lincity-ng/trunk@150 8226ee30-1aee-0310-a633-b68f58d1eb45

---
+++ tools/rename_unix.pl
@@ -0,0 +1,68 @@
+#! /usr/bin/perl
+
+@upcase_patlist = ('changes',
+		   'copying',
+		   'faq',
+		   'readme',
+		   'todo');
+
+@unixify_patlist = ('*.c',
+		    '*.h',
+		    '*.6',
+		    '*.ac',
+		    '*.am',
+		    '*.in',
+		    '*.sh',
+		    '*.rc',
+		    '*.m4',
+		    '*.pal',
+		    'depcomp',
+		    'missing',
+		    'install-sh',
+		    'mkinstalldirs',
+		    'configure',
+		    'README*',
+		    'Make*',
+		    'help/*.hlp',
+		    'messages/*.mes',
+		    'opening/do_image',
+		    'opening/open.pov',
+		    'opening/text*',
+		    'tools/*.c',
+		    'tools/*.pl',
+		    'tools/*.bat',
+		    'tools/*.sh',
+		    'tools/*.pal',
+		    'tools/*.txt',
+		    'tools/fontcvt/*.pl',
+		    'tools/fontcvt/console8x8/*.bdf',
+		    'tools/fontcvt/public9x15/*.bdf',
+		    );
+
+for $pat (@upcase_patlist) {
+    for $file (`ls $pat 2> /dev/null`) {
+	chomp($file);
+	$newname = $file;
+	$newname =~ tr/[a-z]/[A-Z]/;
+	$cmd = "mv $file $newname";
+	print "$cmd\n";
+	print `$cmd\n`;
+    }
+}
+
+for $pat (@unixify_patlist) {
+    for $file (`ls $pat`) {
+	chomp($file);
+	if ($file =~ m/~$/) {
+	    $cmd = "rm $file";
+	} else {
+	    ## Note -- need to save the mode?
+	    $mode = (stat($file))[2];
+	    $mode = $mode & 07777;
+	    $mode = sprintf("%o",$mode);
+	    $cmd = "dos2unix $file;chmod $mode $file";
+	}
+	print "$cmd\n";
+	print `$cmd\n`;
+    }
+}
