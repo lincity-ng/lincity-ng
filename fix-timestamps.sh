620f751eca24effe12ee87b12a4415e69d0eeca6
gregsharp <gregsharp@8226ee30-1aee-0310-a633-b68f58d1eb45>
2003-01-20 06:34:07 +0000

    Upgrade for new autoconf/automake.
    git-svn-id: svn://svn.berlios.de/lincity-ng/trunk@147 8226ee30-1aee-0310-a633-b68f58d1eb45

--- fix-timestamps.sh
+++ fix-timestamps.sh
@@ -1,11 +1,13 @@
 touch Makefile.am
 sleep 2
-touch configure.in
+## touch configure.in
+touch configure.ac
 sleep 2
 touch aclocal.m4
 sleep 2
 touch Makefile.in
 sleep 2
-touch stamp-h.in
+## touch stamp-h.in
+touch stamp-h1
 sleep 2
 touch configure
