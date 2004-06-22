f0e64d5fcad0bf286a0ea31392d27547e9a408a4
immunizer <immunizer@8226ee30-1aee-0310-a633-b68f58d1eb45>
2004-06-22 08:34:23 +0000

    Initial split of module routines into individual source files.  engine.c is one fourth its former size.  modules/* holds everything, including new_module.sh which creates a new module fileset from templates.  It's simple, treat it nice.
    git-svn-id: svn://svn.berlios.de/lincity-ng/trunk@228 8226ee30-1aee-0310-a633-b68f58d1eb45

---
+++ module_skel.c
@@ -0,0 +1,13 @@
+/* ---------------------------------------------------------------------- *
+ * skel.c
+ * This file is part of lincity.
+ * Lincity is copyright (c) I J Peters 1995-1997, (c) Greg Sharp 1997-2001.
+ * (c) Corey Keasling, 2004
+ * ---------------------------------------------------------------------- */
+
+#include <lin-city.h>
+#include <lctypes.h>
+#include <engglobs.h>
+#include <cliglobs.h>
+#include <stats.h>
+#include <skel.h>
