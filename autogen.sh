e4624e5a0072a6cc6249d3e6935688d98d202301
matzebraun <matzebraun@8226ee30-1aee-0310-a633-b68f58d1eb45>
2005-04-25 17:02:54 +0000

    forgot autogen.sh
    git-svn-id: svn://svn.berlios.de/lincity-ng/trunk@557 8226ee30-1aee-0310-a633-b68f58d1eb45

---
+++ autogen.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Correct working directory?
+if test ! -f configure.ac ; then
+  echo "*** Please invoke this script from directory containing configure.ac."
+  exit 1
+fi
+
+autoheader
+aclocal -I mk/autoconf
+
+# generate Jamconfig.in
+autoconf --trace=AC_SUBST \
+  | sed -e 's/configure.ac:[0-9]*:AC_SUBST:\([^:]*\).*/\1 ?= "@\1@" ;/g' \
+  > Jamconfig.in
+sed -e 's/.*BACKSLASH.*//' -i~ Jamconfig.in
+rm Jamconfig.in~
+echo 'INSTALL ?= "@INSTALL@" ;' >> Jamconfig.in
+echo 'JAMCONFIG_READ = yes ;' >> Jamconfig.in
+
+autoconf
+
