d39bb5426c13641dfb681b675ab970e75b5c8cc9
alainb <alainb@8226ee30-1aee-0310-a633-b68f58d1eb45>
2007-12-07 22:59:01 +0000

    update personnal email and (c), add automatic generated CREDITS file  Proposes 1.91.beta as number for next release (file format change needs to be 2.0)
    git-svn-id: svn://svn.berlios.de/lincity-ng/trunk@1310 8226ee30-1aee-0310-a633-b68f58d1eb45

--- makerelease.sh
+++ makerelease.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+cat data/gui/creditslist.xml |grep -v "@"|cut -d\> -f2|cut -d\< -f1 >CREDITS
+echo "# automatically generated from data/gui/creditslist.xml. Do not edit. #">>CREDITS
 
 jam maintainer-clean
 rm -rf build
