c4cbde162b4771dda7ff78e836be0e2f17f7f0dc
dbear496 <70439542+dbear496@users.noreply.github.com>
2024-07-08 14:31:34 -0400

    fix blacksmith skipping monthly update

--- src/lincity/modules/blacksmith.cpp
+++ src/lincity/modules/blacksmith.cpp
@@ -14,17 +14,17 @@
 #include "modules.h"
 
 BlacksmithConstructionGroup blacksmithConstructionGroup(
-    GROUP_BLACKSMITH_NAME,
-    GROUP_BLACKSMITH_NEED_CREDIT,
-    GROUP_BLACKSMITH,
-    GROUP_BLACKSMITH_SIZE,
-    GROUP_BLACKSMITH_COLOUR,
-    GROUP_BLACKSMITH_COST_MUL,
-    GROUP_BLACKSMITH_BUL_COST,
-    GROUP_BLACKSMITH_FIREC,
-    GROUP_BLACKSMITH_COST,
-    GROUP_BLACKSMITH_TECH,
-    GROUP_BLACKSMITH_RANGE
+  GROUP_BLACKSMITH_NAME,
+  GROUP_BLACKSMITH_NEED_CREDIT,
+  GROUP_BLACKSMITH,
+  GROUP_BLACKSMITH_SIZE,
+  GROUP_BLACKSMITH_COLOUR,
+  GROUP_BLACKSMITH_COST_MUL,
+  GROUP_BLACKSMITH_BUL_COST,
+  GROUP_BLACKSMITH_FIREC,
+  GROUP_BLACKSMITH_COST,
+  GROUP_BLACKSMITH_TECH,
+  GROUP_BLACKSMITH_RANGE
 );
 
 Construction *BlacksmithConstructionGroup::createConstruction(int x, int y) {
@@ -33,38 +33,36 @@ Construction *BlacksmithConstructionGroup::createConstruction(int x, int y) {
 
 void Blacksmith::update()
 {
-    if (pauseCounter++ < 0)
-    {   return;}
-    if ((commodityCount[STUFF_GOODS] + GOODS_MADE_BY_BLACKSMITH <= MAX_GOODS_AT_BLACKSMITH )
-        && (commodityCount[STUFF_COAL] >= BLACKSMITH_COAL_USED)
-        && (commodityCount[STUFF_STEEL] >= BLACKSMITH_STEEL_USED)
-        && (commodityCount[STUFF_LABOR] >= BLACKSMITH_LABOR))
-    {
-        produceStuff(STUFF_GOODS, GOODS_MADE_BY_BLACKSMITH);
-        consumeStuff(STUFF_COAL, BLACKSMITH_COAL_USED);
-        consumeStuff(STUFF_STEEL, BLACKSMITH_STEEL_USED);
-        consumeStuff(STUFF_LABOR, BLACKSMITH_LABOR);
-        working_days++;
-        if ((goods_made += GOODS_MADE_BY_BLACKSMITH) >= BLACKSMITH_BATCH)
-        {
-            animate_enable = true;
-            world(x,y)->pollution++;
-            goods_made = 0;
-        }
+  if(pauseCounter++ < 0)
+    ;
+  if(commodityCount[STUFF_GOODS] + GOODS_MADE_BY_BLACKSMITH
+      <= MAX_GOODS_AT_BLACKSMITH
+    && commodityCount[STUFF_COAL] >= BLACKSMITH_COAL_USED
+    && commodityCount[STUFF_STEEL] >= BLACKSMITH_STEEL_USED
+    && commodityCount[STUFF_LABOR] >= BLACKSMITH_LABOR
+  ) {
+    produceStuff(STUFF_GOODS, GOODS_MADE_BY_BLACKSMITH);
+    consumeStuff(STUFF_COAL, BLACKSMITH_COAL_USED);
+    consumeStuff(STUFF_STEEL, BLACKSMITH_STEEL_USED);
+    consumeStuff(STUFF_LABOR, BLACKSMITH_LABOR);
+    working_days++;
+    if ((goods_made += GOODS_MADE_BY_BLACKSMITH) >= BLACKSMITH_BATCH) {
+      animate_enable = true;
+      world(x,y)->pollution++;
+      goods_made = 0;
     }
-    else
-    {
-        animate_enable = false;
-        pauseCounter = -BLACKSMITH_CLOSE_TIME;
-        return;
-    }
+  }
+  else {
+    animate_enable = false;
+    pauseCounter = -BLACKSMITH_CLOSE_TIME;
+  }
 
-    //monthly update
-    if (total_time % 100 == 99) {
-        reset_prod_counters();
-        busy = working_days;
-        working_days = 0;
-    }
+  //monthly update
+  if (total_time % 100 == 99) {
+    reset_prod_counters();
+    busy = working_days;
+    working_days = 0;
+  }
 }
 
 void Blacksmith::animate() {
@@ -84,12 +82,12 @@ void Blacksmith::animate() {
 
 void Blacksmith::report()
 {
-    int i = 0;
+  int i = 0;
 
-    mps_store_sd(i++, constructionGroup->name, ID);
-    i++;
-    mps_store_sfp(i++, N_("busy"), (float) busy);
-    // i++;
+  mps_store_sd(i++, constructionGroup->name, ID);
+  i++;
+  mps_store_sfp(i++, N_("busy"), (float) busy);
+  // i++;
     list_commodities(&i);
 }
 
