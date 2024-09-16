5ffe0511a292bc22c011fe8ede00f5c17976810e
dbear496 <70439542+dbear496@users.noreply.github.com>
2024-09-16 09:21:18 -0400

    remove binary saving

--- src/lincity-ng/MainMenu.cpp
+++ src/lincity-ng/MainMenu.cpp
@@ -317,8 +317,8 @@ MainMenu::loadOptionsMenu()
         currentCheckButton->clicked.connect( makeCallback(*this, &MainMenu::optionsMenuButtonClicked));
         currentCheckButton = getCheckButton(*optionsMenu, "LanguageNext");
         currentCheckButton->clicked.connect( makeCallback(*this, &MainMenu::optionsMenuButtonClicked));
-        currentCheckButton = getCheckButton(*optionsMenu, "BinaryMode");
-        currentCheckButton->clicked.connect( makeCallback(*this, &MainMenu::optionsMenuButtonClicked));
+        // currentCheckButton = getCheckButton(*optionsMenu, "BinaryMode");
+        // currentCheckButton->clicked.connect( makeCallback(*this, &MainMenu::optionsMenuButtonClicked));
         currentCheckButton = getCheckButton(*optionsMenu, "SeedMode");
         currentCheckButton->clicked.connect( makeCallback(*this, &MainMenu::optionsMenuButtonClicked));
 
@@ -341,10 +341,10 @@ MainMenu::loadOptionsMenu()
     } else {
         getCheckButton(*optionsMenu, "Fullscreen")->uncheck();
     }
-    if (binary_mode)
-    {   getCheckButton(*optionsMenu, "BinaryMode")->check();}
-    else
-    {   getCheckButton(*optionsMenu, "BinaryMode")->uncheck();}
+    // if (binary_mode)
+    // {   getCheckButton(*optionsMenu, "BinaryMode")->check();}
+    // else
+    // {   getCheckButton(*optionsMenu, "BinaryMode")->uncheck();}
     if (seed_compression)
     {   getCheckButton(*optionsMenu, "SeedMode")->check();}
     else
@@ -600,8 +600,8 @@ void MainMenu::optionsMenuButtonClicked( CheckButton* button, int ){
         changeTrack(false);
     } else if( buttonName == "TrackNext"){
         changeTrack(true);
-    } else if( buttonName == "BinaryMode"){
-        binary_mode = !binary_mode;
+    // } else if( buttonName == "BinaryMode"){
+    //     binary_mode = !binary_mode;
     } else if( buttonName == "SeedMode"){
         seed_compression = !seed_compression;
     } else {
