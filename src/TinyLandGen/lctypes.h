7cddfac9f63edee82c7935f534c311e3a17f2de1
bufalo1973 <59099667+bufalo1973@users.noreply.github.com>
2025-02-20 21:13:55 +0100

    Update lctypes.h
    Changed C arrays to std::array.
    lmap_struct now is static. lmap is an alias.

--- src/TinyLandGen/lctypes.h
+++ src/TinyLandGen/lctypes.h
@@ -5,6 +5,8 @@
 #ifndef __lctypes_h__
 #define __lctypes_h__
 
+#include <array>
+
 /* Vector for visiting neigbours = ( di(k) , dj(k) )  ; ordered so that diagonal moves are the last 4 */
 static const int di[8] = { -1, 0, 1, 0, 1, 1, -1, -1 };
 static const int dj[8] = { 0, -1, 0, 1, 1, -1, 1, -1 };
@@ -23,35 +25,42 @@ enum {
 
 //gui_interface/shared_globals.h:
 	NUMOF_MODULES = 32
-};
 
 // src/lincity/lctypes.h  CST_stuff
-enum {
 	GREEN	= 0,
 	WATER	= 221,
 	DESERT	= 350,
 	TREE	= 351,
 	TREE2	= 352,
 	TREE3	= 353
-};
 
 // src/lincity/loadsave.h
-enum { WATERWELL_V2 = 1322 };
+	WATERWELL_V2 = 1322,
 
 // src/lincity/lintypes.h
-enum {
 	MAX_NUMOF_SUBSTATIONS = 512,
-	MAX_NUMOF_MARKETS = 512
+	MAX_NUMOF_MARKETS = 512,
+
+// src/lincity/lin-city.h
+	FLAG_IS_RIVER			= 0x800000,
+	FLAG_HAS_UNDERGROUND_WATER	= 0x20000000
 };
 
 struct lmap_struct {
-	float altitude[WORLD_SIDE_LEN][WORLD_SIDE_LEN];
-	unsigned long color[WORLD_SIDE_LEN][WORLD_SIDE_LEN];
-	int type[WORLD_SIDE_LEN][WORLD_SIDE_LEN];
-	int flag[WORLD_SIDE_LEN][WORLD_SIDE_LEN];
-	int dist2w[WORLD_SIDE_LEN][WORLD_SIDE_LEN]; // distance to water; manhattan (L1) is fast to compute iteratively
-    	int ecotable[WORLD_SIDE_LEN][WORLD_SIDE_LEN]; //
+	static std::array<std::array<float, WORLD_SIDE_LEN>, WORLD_SIDE_LEN> altitude;
+	static std::array<std::array<unsigned long, WORLD_SIDE_LEN>, WORLD_SIDE_LEN> color;
+	static std::array<std::array<int, WORLD_SIDE_LEN>, WORLD_SIDE_LEN> type;
+	static std::array<std::array<int, WORLD_SIDE_LEN>, WORLD_SIDE_LEN> flag;
+	static std::array<std::array<int, WORLD_SIDE_LEN>, WORLD_SIDE_LEN> dist2w; // distance to water; manhattan (L1) is fast to compute iteratively
+	static std::array<std::array<int, WORLD_SIDE_LEN>, WORLD_SIDE_LEN> ecotable; //
 };
+
+using lmap = lmap_struct;
+
+float &ALT(int x, int y) { return lmap::altitude.at(x).at(y); }
+int &MP_TYPE(int x, int y)   { return lmap::type.at(x).at(y); }
+int &MP_FLAG(int x, int y)   { return lmap::flag.at(x).at(y); }
+unsigned long &MP_COLOR(int x, int y)	{ return lmap::color.at(x).at(y); }
 
 float ALT(int x, int y)			{ return lmap.altitude[x][y]; }
 int MP_TYPE(int x, int y)		{ return lmap.type[x][y]; }
@@ -59,15 +68,9 @@ int MP_FLAG(int x, int y)		{ return lmap.flag[x][y]; }
 unsigned long MP_COLOR(int x, int y)	{ return lmap.color[x][y]; }
 
 //src/lincity/power.h:
-bool IS_WATER(x,y)    { return MP_TYPE(x,y) == WATER; }
+bool IS_WATER(int x, int y)    { return MP_TYPE(x,y) == WATER; }
 
 //src/lincity/init_game.cpp:
 bool IS_RIVER(int x, inty) { return MP_FLAG(x,y) & FLAG_IS_RIVER; }
-
-// src/lincity/lin-city.h
-enum {
-	FLAG_IS_RIVER			= 0x800000,
-	FLAG_HAS_UNDERGROUND_WATER	= 0x20000000
-};
 
 #endif
