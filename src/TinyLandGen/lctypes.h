654788762c8dde7c97b6dbc5c17bbf2850be66ad
bufalo1973 <59099667+bufalo1973@users.noreply.github.com>
2025-02-20 20:44:44 +0100

    Update lctypes.h
    Changed  define values to enums and macro functions to functions.

--- src/TinyLandGen/lctypes.h
+++ src/TinyLandGen/lctypes.h
@@ -9,34 +9,40 @@
 static const int di[8] = { -1, 0, 1, 0, 1, 1, -1, -1 };
 static const int dj[8] = { 0, -1, 0, 1, 1, -1, 1, -1 };
 
-#define WORLD_SIDE_LEN 100
-#define SIZE 8
-#define SCREEN_SIZE (SIZE * WORLD_SIDE_LEN)
+enum {
+	WORLD_SIDE_LEN = 100,
+	SIZE = 8,
+	SCREEN_SIZE = SIZE * WORLD_SIDE_LEN
 
 /*
  * needed to save in LinCity-NG format
  */
 //gui_interface/pbar_interface.h:
-#define PBAR_DATA_SIZE 12
-#define NUM_PBARS 9
+	PBAR_DATA_SIZE = 12,
+	NUM_PBARS = 9,
 
 //gui_interface/shared_globals.h:
-#define NUMOF_MODULES 32
+	NUMOF_MODULES = 32
+};
 
 // src/lincity/lctypes.h  CST_stuff
-#define GREEN		0
-#define WATER		221
-#define DESERT		350
-#define TREE		351
-#define TREE2		352
-#define TREE3		353
+enum {
+	GREEN	= 0,
+	WATER	= 221,
+	DESERT	= 350,
+	TREE	= 351,
+	TREE2	= 352,
+	TREE3	= 353
+};
 
 // src/lincity/loadsave.h
-#define WATERWELL_V2 1322
+enum { WATERWELL_V2 = 1322 };
 
 // src/lincity/lintypes.h
-#define MAX_NUMOF_SUBSTATIONS 512
-#define MAX_NUMOF_MARKETS 512
+enum {
+	MAX_NUMOF_SUBSTATIONS = 512,
+	MAX_NUMOF_MARKETS = 512
+};
 
 struct lmap_struct {
 	float altitude[WORLD_SIDE_LEN][WORLD_SIDE_LEN];
@@ -47,19 +53,21 @@ struct lmap_struct {
     	int ecotable[WORLD_SIDE_LEN][WORLD_SIDE_LEN]; //
 };
 
-#define ALT(x,y) lmap.altitude[x][y]
-#define MP_TYPE(x,y)   lmap.type[x][y]
-#define MP_FLAG(x,y)   lmap.flag[x][y]
-#define MP_COLOR(x,y)	lmap.color[x][y]
+float ALT(int x, int y)			{ return lmap.altitude[x][y]; }
+int MP_TYPE(int x, int y)		{ return lmap.type[x][y]; }
+int MP_FLAG(int x, int y)		{ return lmap.flag[x][y]; }
+unsigned long MP_COLOR(int x, int y)	{ return lmap.color[x][y]; }
 
 //src/lincity/power.h:
-#define IS_WATER(x,y)    (MP_TYPE(x,y) == WATER)
+bool IS_WATER(x,y)    { return MP_TYPE(x,y) == WATER; }
 
 //src/lincity/init_game.cpp:
-#define IS_RIVER(x,y) (MP_FLAG(x,y) & FLAG_IS_RIVER)
+bool IS_RIVER(int x, inty) { return MP_FLAG(x,y) & FLAG_IS_RIVER; }
 
 // src/lincity/lin-city.h
-#define FLAG_IS_RIVER           (0x800000)
-#define FLAG_HAS_UNDERGROUND_WATER (0x20000000)
+enum {
+	FLAG_IS_RIVER			= 0x800000,
+	FLAG_HAS_UNDERGROUND_WATER	= 0x20000000
+};
 
 #endif
